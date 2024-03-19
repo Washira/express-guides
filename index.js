55f69af0edb2abcf53f651e2124493c6e98ab6e2
washira <washira.suyajai@gmail.com>
2024-03-19 22:52:35 +0700

    ✨ (index.js): Add new API endpoints for user login, user registration, and fetching users 🔧 (package.json): Add a start script to run a Python HTTP server for serving the index.html file 📝 (index.html): Add a basic login form and functionality to send requests to the new API endpoints

--- index.js
+++ index.js
@@ -56,7 +56,65 @@ app.post('/api/register', async (req, res) => {
     const [result] = await conn.query('INSERT INTO users SET ?', useData)
     res.json({ message: 'Creating has been successfully', result })
   } catch (error) {
-    res.json({ message: error.message })
+    res.json({ message: error.message, error })
+  }
+})
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const [rows] = await conn.query(
+      'SELECT * FROM users WHERE email = ?',
+      email
+    )
+    if (rows.length === 0) {
+      return res.json({ message: 'Email not found' })
+    }
+    const user = rows[0]
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      return res.json({ message: 'User or password not match' })
+    }
+    // jwt token
+    const token = jwt.sign({ email: user.email }, secret, {
+      expiresIn: '1h',
+    })
+
+    res.cookie('token', token, {
+      maxAge: 100 * 60 * 5, // the token will be removed out from browser after 5 minutes
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none', // for cross-site request
+    })
+
+    res.json({ message: 'Login successfully', token })
+  } catch (error) {
+    res.status(401).json({ message: 'login fail', error })
+  }
+})
+
+app.get('/api/users', async (req, res) => {
+  try {
+    // const token = req?.headers?.authorization?.split(' ')[1] // Bearer token, get token from headers
+    const token = req.cookies.token // get token from cookies
+    if (!token) {
+      return res.json({ message: 'Token not found' })
+    }
+    const decoded = jwt.verify(token, secret) // decode token
+    if (!decoded) {
+      return res.json({ message: 'Token not valid' })
+    }
+    const [checkUser] = await conn.query(
+      'SELECT * FROM users WHERE email = ?',
+      decoded.email
+    ) // check user
+    if (checkUser.length === 0) {
+      return res.json({ message: 'User not found' })
+    }
+    const [rows] = await conn.query('SELECT * FROM users')
+    res.json({ users: rows })
+  } catch (error) {
+    res.json({ message: error.message, error })
   }
 })
 
